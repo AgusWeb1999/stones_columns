45b61aa79e2f29b067c4e20f6db908e9fd65a4e9
maximiliano836 <maxi10.02.00.g@gmail.com>
2025-04-11 22:13:43 -0300

    finalizando nav

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/NavComponent.css'; 
-import logo from '../images/logo_principal.png';
-import logo_secundario from '../images/logo_secundario.png';
+import logo from '../images/Stone-Logo-Outline.png';
 const NavComponent = () => {
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
         <img src={logo} alt="Logo principal" />
       </Link>      
-      <Link to="/" className="logo_secundario">
-        <img src={logo_secundario} alt="Logo principal" />
-      </Link>   
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/services">Galery</Link></li>
         <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
